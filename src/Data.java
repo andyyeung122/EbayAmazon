3dd89d18d39c8199d7632b7940f0a88fb6068724
coollama1 <lamemarvin@gmail.com>
2019-05-06 10:25:29 -0400

    fixing sql import

--- src/Data.java
+++ src/Data.java
@@ -1,4 +1,4 @@
-import java.sql;
+import java.sql.*;
 
 public class Data{
     static{
@@ -14,10 +14,11 @@ OrdinaryUser(username:str primKey/frgn key, password frgn key, address:str, crei
 Superuser(username:str primKey, password:str, name:str)
 
 Item(id:int primKey, name:str, registered:bool, imageURL/file:str, associatedKeywords:str) 
-BidItem(id:int primKey/frgn key, highestBid: str)//update highest bid whenever bid is added; default bid duration: 3 min
-FixedItem(id:int primKey/frgn key, fixedPrice: str)
+FixedItem(id:int primKey/frgn key, fixedPrice: float) //price is input as string, then converted to float
+BidItem(id:int primKey/frgn key, highestBid: float, startOfBid:long)//update highest bid whenever bid is added; default bid duration: 3 min; startOfBid -> timestamp in milliseconds
+Bid(itemID:int frgn key, user:str frgn key, amount:float)
 
-Purchase(itemID: int primKey/frgn key, seller:str frgn key, buyer:str frgn key, price: str)
+Purchase(itemID: int primKey/frgn key, seller:str frgn key, buyer:str frgn key, price: float)
 Rating(itemID: int frgn key, reviewer:str frgn key, seller:str frgn key, rating:float, review:str) primKey(itemID, reviewer)//if rating for (itemID, reviewer) already exists, do not prompt for review/rating
 Notification(id:int primKey, title:str, message:str, receiver:str frgn key, timeStamp:long, warning:bool) //timestamp should be in milliseconds
 Complaint(id:int primKey, title:str, message:str, sender:str frgn key, handled:bool)
