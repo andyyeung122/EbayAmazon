14fb60aedd660749db4013c0ddb9ba219aeec9ae
FIRST_NAME LAST_NAME <aqsamalik70@gmail.com>
2019-05-14 13:44:16 -0400

    change ViewComplaints

--- src/ViewComplaints.java
+++ src/ViewComplaints.java
@@ -1,2 +1,50 @@
-public class ViewComplaints {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
+public class ViewComplaints  extends Application {
+    public ViewComplaints() {
+    }
+
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle(" Super User!");
+        Label title = new Label("Complaints");
+
+        TableView compalinTable = new TableView();
+        TableColumn userName =  new TableColumn();
+
+
+
+
+        Button viewCompalin = new Button();
+        viewCompalin.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                String user = userName.getText();
+
+
+            }
+        });
+
+        Button back = new Button("Back");
+        back.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                SuperHomePage sh = new SuperHomePage();
+                sh.start(primaryStage);
+
+            }
+
+
+        });
+
+
+
+
+    }
 }
