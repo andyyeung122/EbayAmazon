5d4017e57bb0209c7e4b9353d28f332e3b37d111
coollama1 <lamemarvin@gmail.com>
2019-05-14 14:48:10 -0400

    readding driver

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -76,12 +76,8 @@ public class LoginPage extends Scene {
 
         // Connect to Database
         login.setOnAction( e -> {
-<<<<<<< HEAD
             if(!data.isSuperUser(userNameTextField.getText(),passWordTextField.getText())&&data.isOrdinairyUser(userNameTextField.getText(),passWordTextField.getText())) {
                 main.setUsername(userNameTextField.getText());
-=======
-            if(!data.isSuperUser(userNameTextField.getText(),passWordTextField.getText())&&data.isOrdinairyUser(userNameTextField.getText(),passWordTextField.getText())&&data.isRegisteredUser(userNameTextField.getText())==true) {
->>>>>>> 77b5efe132bcced2e5fe90ab6bd3d01442c09b44
                 main.getPrimaryStage().setScene(ordhomepage);
                 main.getPrimaryStage().setTitle("Home Page");
                 main.getPrimaryStage().show();
