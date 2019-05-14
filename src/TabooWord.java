084fb787d2062677f05c8b4a3adadfd2bd5ee580
FIRST_NAME LAST_NAME <aqsamalik70@gmail.com>
2019-05-14 13:48:18 -0400

    application

--- src/TabooWord.java
+++ src/TabooWord.java
@@ -2,7 +2,10 @@ import javafx.application.Application;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -10,9 +13,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class TabooWord extends Application {
@@ -22,43 +27,62 @@ public class TabooWord extends Application {
     public TabooWord() {
     }
 
-    public void start(Stage stage) {
-        Scene scene = new Scene(new Group());
-        stage.setTitle("Ebay Amazon");
-        stage.setWidth(400.0D);
-        stage.setHeight(600.0D);
+    public void start(Stage primaryStage) {
+
+        primaryStage.setTitle("Ebay Amazon");
+        GridPane tabooGrid = new GridPane();
+        tabooGrid.setAlignment(Pos.CENTER);
+        tabooGrid.setHgap(10);
+        tabooGrid.setVgap(10.D);
+        tabooGrid.setPadding(new Insets(25.0D, 25.0D, 25.0D, 25.0D));
+        Scene scene = new Scene(tabooGrid, 400, 400);
+        Text title = new Text("Taboo Words");
+        tabooGrid.add(title,0,0,2,1);
+        title.setFont(Font.font("Verdana", 20.0D));
         Label nameLabel = new Label("Taboo Words");
-        nameLabel.setFont(new Font("Roman", 20.0D));
+        nameLabel.setFont(new Font("Roman", 20));
         TextField nameTxt = new TextField();
         nameTxt.setPromptText("Name");
         ListView myListView = new ListView();
         myListView.setItems(this.WordsList);
         myListView.setOnMouseClicked((event) -> {
-            //String selectedItem = myListView.getSelectionModel().getSelectedItem().toString();
             this.selectedIndex = myListView.getSelectionModel().getSelectedIndex();
 
         });
-        Button addButton = new Button("Add");
-        addButton.setOnAction((e) -> {
+
+        tabooGrid.add(myListView, 1,2);
+
+        Button add = new Button("Add");
+        add.setOnAction((e) -> {
             this.WordsList.add(new TabooWord.WordList(nameTxt.getText()));
             nameTxt.clear();
         });
-        Button deleteBtn = new Button("Delete");
-        deleteBtn.setOnAction((e) -> {
+        Button delete= new Button("Delete");
+        delete.setOnAction((e) -> {
             this.WordsList.remove(this.selectedIndex);
             nameTxt.clear();
         });
-        HBox myHBox = new HBox();
-        myHBox.getChildren().addAll(new Node[]{nameTxt, addButton, deleteBtn});
-        myHBox.setSpacing(3.0D);
-        VBox myVBox = new VBox();
-        myVBox.setSpacing(5.0D);
-        myVBox.setPadding(new Insets(10.0D, 0.0D, 0.0D, 10.0D));
-        myVBox.getChildren().addAll(new Node[]{nameLabel, myListView, myHBox});
-        ((Group)scene.getRoot()).getChildren().addAll(new Node[]{myVBox});
-        stage.setScene(scene);
-        stage.show();
+
+        Button back = new Button("Back");
+        back.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                SuperHomePage sh = new SuperHomePage();
+                sh.start(primaryStage);
+
+            }
+        });
+
+        HBox allButton = new HBox(10);
+        allButton.setAlignment(Pos.BOTTOM_LEFT);
+        allButton.getChildren().addAll(add,delete,back);
+        tabooGrid.add(allButton,1,4);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
+
+
     public static class WordList {
         private final SimpleStringProperty name;
 
