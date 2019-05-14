e958dc84bdfb8921d3eada6c4f8c915f557cb3f0
FIRST_NAME LAST_NAME <aqsamalik70@gmail.com>
2019-05-14 13:51:09 -0400

    change SuperHomePage

--- src/SuperHomePage.java
+++ src/SuperHomePage.java
@@ -27,14 +27,82 @@ public class SuperHomePage extends Application {
         userFunction.setAlignment(Pos.CENTER);
         userFunction.setHgap(10.0D);
         userFunction.setVgap(10.D);
+
+
+        // functions that super user can perform
+
+        // view application for ordinary user
         Button viewApplication = new Button("View Application");
+        viewApplication.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                ViewApplication va = new ViewApplication();
+                va.start(primaryStage);
+
+            }
+        });
+
+
+        // accept or reject the processing items
         Button ViewProcessingItem = new Button("View Processing Items");
+        ViewProcessingItem.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                ViewProcessingItem pt = new ViewProcessingItem();
+                pt.start(primaryStage);
+
+            }
+        });
+
+        //Send message to ordinary user
         Button sendMessage = new Button(" Send Message");
+        sendMessage.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                SendMessage sm = new SendMessage();
+                sm.start(primaryStage);
+
+            }
+        });
+
+
+        // view transaction history of item sold or purchase
         Button viewHistory = new Button(" View History");
+        viewHistory.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                ViewTransactionHistory history = new ViewTransactionHistory();
+                history.start(primaryStage);
+
+            }
+        });
+
+        // list of taboo words
         Button tabooWord = new Button(" Taboo Word");
+        tabooWord.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                TabooWord tw= new TabooWord();
+                tw.start(primaryStage);
+
+            }
+        });
+
+        //delete user and sen them message
         Button deleteUser = new Button("Delete User");
-        Button outstandingUser = new Button("Outstanding User");
+        deleteUser.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                DeleteUser du = new DeleteUser();
+                du.start(primaryStage);
+
+            }
+        });
+
+
+        //view complaints from ordinary user and re
         Button viewComplaints = new Button("View Complaints");
+
 
         userFunction.add(viewApplication,1,0);
         userFunction.add(ViewProcessingItem,1,1);
@@ -42,8 +110,7 @@ public class SuperHomePage extends Application {
         userFunction.add(viewHistory,1,3);
         userFunction.add(tabooWord,1,4);
         userFunction.add(deleteUser,1,5);
-        userFunction.add(outstandingUser,1,6);
-        userFunction.add(viewComplaints,1,7);
+        userFunction.add(viewComplaints,1,6);
 
         BorderPane borderPane = new BorderPane();
         borderPane.setTop(topLabel);
